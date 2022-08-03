aaca10f7f977d62e12fd40f58612b6bb15cc1277
Junior Mayta <junior@Juniors-MBP-2.fritz.box>
2022-08-03 18:19:01 +0200

    Renaming base project to a more generic one.

--- location_advisor/settings.py
+++ location_advisor/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'arrivedo.urls'
+ROOT_URLCONF = 'location_advisor.urls'
 
 TEMPLATES = [
     {
@@ -74,7 +74,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'arrivedo.wsgi.application'
+WSGI_APPLICATION = 'location_advisor.wsgi.application'
 
 
 # Database
--- location_advisor/wsgi.py
+++ location_advisor/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "arrivedo.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "location_advisor.settings")
 
 application = get_wsgi_application()
